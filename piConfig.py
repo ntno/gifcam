2f4f3bc374f3894859569fd403b97cbd45c5c123
ntno <organick.natan@gmail.com>
2019-07-04 12:27:39 -0400

    testing LEDs

--- piConfig.py
+++ piConfig.py
@@ -12,24 +12,35 @@ GPIO.setwarnings(False)
 button = 19 #Button GPIO Pin
 GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-led_1 = 10 #Status LED GPIO Pin
+led_1 = 10 
 GPIO.setup(led_1, GPIO.OUT)
-buttonLed = GPIO.PWM(led_1, 10)
+
+led_2 = 12
+GPIO.setup(led_2, GPIO.OUT)
+
+buttonLed = GPIO.PWM(led_2, led_2)
 buttonLed.start(0)
 
-led_2 = 12 #ON/OFF LED Pin
-GPIO.setup(led_2, GPIO.OUT)
-statusLed = GPIO.PWM(led_2, 12)
+statusLed = GPIO.PWM(led_1, led_1)
 statusLed.start(0)
 
-def turnOnButtonLight():
-    buttonLed.ChangeDutyCycle(50)
+FLASH_RATE=20
+ON_RATE=100
+
+def turnOnButtonLight(onRate=ON_RATE):
+    buttonLed.ChangeDutyCycle(onRate)
+
+def flashButtonLight(flashRate=FLASH_RATE):
+    buttonLed.ChangeDutyCycle(flashRate)
 
 def turnOffButtonLight():
     buttonLed.ChangeDutyCycle(0)
 
-def turnOnStatusLight():
-    statusLed.ChangeDutyCycle(50)
+def turnOnStatusLight(onRate=ON_RATE):
+    statusLed.ChangeDutyCycle(onRate)
+
+def flashStatusLight(flashRate=FLASH_RATE):
+    statusLed.ChangeDutyCycle(flashRate)
 
 def turnOffStatusLight():
     statusLed.ChangeDutyCycle(0)
