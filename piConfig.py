08e93592087f975182bf0f41f7d355c69d1c8bcf
ntno <organick.natan@gmail.com>
2019-07-04 13:33:34 -0400

    refactor

--- piConfig.py
+++ piConfig.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
 
-
 ########################
 #
 # Define GPIO
@@ -9,38 +8,36 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-button = 19 #Button GPIO Pin
-GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+button_pin = 19 #Button GPIO Pin
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-led_1 = 10 
-GPIO.setup(led_1, GPIO.OUT)
-
-led_2 = 12
-GPIO.setup(led_2, GPIO.OUT)
-
-buttonLed = GPIO.PWM(led_2, 1)
-buttonLed.start(0)
-
-statusLed = GPIO.PWM(led_1, 1)
+status_led_pin = 10 
+GPIO.setup(status_led_pin, GPIO.OUT)
+statusLed = GPIO.PWM(status_led_pin, 2)
 statusLed.start(0)
 
-FLASH_RATE=20
-ON_RATE=100
+button_led_pin = 12
+GPIO.setup(button_led_pin, GPIO.OUT)
+buttonLed = GPIO.PWM(button_led_pin, 2)
+buttonLed.start(0)
 
-def turnOnButtonLight(onRate=ON_RATE):
-    buttonLed.ChangeDutyCycle(onRate)
+def isButtonPressed():
+    return GPIO.input(button_pin) == False
 
-def flashButtonLight(flashRate=FLASH_RATE):
-    buttonLed.ChangeDutyCycle(flashRate)
+def turnOnButtonLight():
+    buttonLed.ChangeDutyCycle(100)
+
+def flashButtonLight():
+    buttonLed.ChangeDutyCycle(50)
 
 def turnOffButtonLight():
     buttonLed.ChangeDutyCycle(0)
 
-def turnOnStatusLight(onRate=ON_RATE):
-    statusLed.ChangeDutyCycle(onRate)
+def turnOnStatusLight():
+    statusLed.ChangeDutyCycle(100)
 
-def flashStatusLight(flashRate=FLASH_RATE):
-    statusLed.ChangeDutyCycle(flashRate)
+def flashStatusLight():
+    statusLed.ChangeDutyCycle(50)
 
 def turnOffStatusLight():
     statusLed.ChangeDutyCycle(0)
