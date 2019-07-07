6a3c8369a6f0d815a5f1ea5eb953002f4c68f139
ntno <organick.natan@gmail.com>
2019-07-07 16:16:58 -0400

    more experiments

--- lambda/generate-s3-url/generateUrl.py
+++ lambda/generate-s3-url/generateUrl.py
@@ -1,20 +1,29 @@
-import os
-import logging
-import boto3
-import uuid
-import requests
+import os, logging, boto3, uuid, requests, copy, json
 from botocore.exceptions import ClientError
 
 BUCKET_NAME = os.getenv('BUCKET_NAME')
 PUT_PREFIX = os.getenv('PUT_PREFIX')
-URL_TIMEOUT = os.getenv('URL_TIMEOUT')
+# URL_TIMEOUT = os.getenv('URL_TIMEOUT')
 
 S3_CLIENT = boto3.client('s3')
+LOGGER = logging.getLogger(__name__)
 
-def generateFileName():
+def generateFolderName():
     return '{}'.format(str(uuid.uuid1()))
 
-def create_presigned_post(bucket_name=BUCKET_NAME, object_name=generateFileName(),
+def generateS3Prefix(folderName):
+    return '{}/{}/'.format(PUT_PREFIX, folderName)
+
+def getFields():
+    return {'x-amz-server-side-encryption': "AES256"}
+
+def getConditions(keyPrefix):
+    serverSideEncryption = {"x-amz-server-side-encryption":"AES256"}
+    prefix = ["starts-with", "$key", keyPrefix]
+    conditions = [prefix, serverSideEncryption]
+    return conditions
+
+def create_presigned_post(object_name, bucket_name=BUCKET_NAME,
                           fields=None, conditions=None, expiration=3600):
     """Generate a presigned URL S3 POST request to upload a file
 
@@ -32,17 +41,14 @@ def create_presigned_post(bucket_name=BUCKET_NAME, object_name=generateFileName(
     # Generate a presigned S3 POST URL
     s3_client = boto3.client('s3')
     try:
-        # if(not fields):
-        #     fields={}
-        # fields['s3:x-amz-server-side-encryption']='AES256'
-        # print("generating with fields=", fields)
+        objectKey = '{}{}'.format(conditions[0][2], object_name)
         response = s3_client.generate_presigned_post(bucket_name,
-                                                     object_name,
+                                                     objectKey,
                                                      Fields=fields,
                                                      Conditions=conditions,
                                                      ExpiresIn=expiration)
     except ClientError as e:
-        logging.error(e)
+        LOGGER.error(e)
         return None
 
     # The response contains the presigned URL and required fields
@@ -50,31 +56,39 @@ def create_presigned_post(bucket_name=BUCKET_NAME, object_name=generateFileName(
 
 def postToPresignedUrl(url, fields, pathToFile):
     with open(pathToFile, 'rb') as f:
-        s3Key = '{}'.format(fields['key'])
-        files = {'file': (s3Key, f)}
+        # s3Key = '{}'.format(fields['key'])
+        files = {'file': ("somestring", f)}
         http_response = requests.post(url, data=fields, files=files)
+    # files = {'file': open(pathToFile, 'rb')}
+    # http_response = requests.post(url, data=fields, files=files)
     return http_response
 
-def testPresignedUrl(bucket_name, object_name, pathToFile):
-    response = create_presigned_post(bucket_name, object_name)
-    if response is None:
-        print("could not generate url")
-    else:
-        print("url", response['url'])
-        print("fields", response['fields'])
-        http_response = postToPresignedUrl(response['url'], response['fields'], pathToFile)
-        print(f'File upload HTTP status code: {http_response.status_code}')
+#you can use the same URL twice to load two different files (but they get saved to the same key)
+def testPresignedUrl(pathToFile):
+    folderName = generateFolderName()
+    s3FilePrefix = generateS3Prefix(folderName)
+    postConditions = getConditions(s3FilePrefix)
 
+    presigned = create_presigned_post("test-0.txt", fields=getFields(), conditions=postConditions)
+    LOGGER.error(json.dumps(presigned))
 
-def lambda_handler(event, context):  
-    filename = generateFileName()
+    httpResponse = postToPresignedUrl(presigned['url'], presigned['fields'], pathToFile)
+    LOGGER.error(httpResponse.status_code)
+    httpResponse.close()
+
+    anotherResponse = postToPresignedUrl(presigned['url'], presigned['fields'], "boto3_examples.py")
+    LOGGER.error(anotherResponse.status_code)
+    anotherResponse.close()
+
+def lambda_handler(event, context):
+    # filename = generatePrefi  x()
     presignedUrl = 'yolo'
 
-    # testPresignedUrl()
+    testPresignedUrl("generateUrl.py")
     # str(create_presigned_url(generateFileName))
     # print(presignedUrl)
     # testUrl(presignedUrl)
-    return {'status':200, 'url': presignedUrl, 'filename' : filename, 'timeout':URL_TIMEOUT}
+    return {'status':200} #, 'url': presignedUrl, 'filename' : filename, 'timeout':URL_TIMEOUT}
 
 if __name__ == "__main__":
     DEBUG=True
